0d9c57b5bb3bcc8813e8bfd89fa4e994997c853d
shota12085 <shota.toyoshima007@gmail.com>
2020-11-12 16:38:51 +0900

    react追加

--- source/resources/views/article/index.blade.php
+++ source/resources/views/article/index.blade.php
@@ -1,38 +1,12 @@
 @extends('../layout')
 @section('content')
-<h1>Article</h1>
-<form action="/article" method="POST">
-  {{ csrf_field()}}
-  <div>
-    <label for="article-title">タイトル</label>
-    <input type="text" name="title" id="article-title">
-  </div>
-  <div>
-    <label for="article-content">内容</label>
-    <input type="text" name="content" id="article-content">
-  </div>
-  <input type="submit">
-</form>
+<div id="app">
+        <div class="container">
+            <h3 class="mt-5">Todo 管理システム</h3>
 
-<h3>Article List</h3>
-<table>
-  <tr>
-    <th>タイトル</th>
-    <th>内容</th>
-  </tr>
-  @foreach ($articles as $article)
-    <tr>
-      <th>{{ $article->title }}</th>
-      <th>{{ $article->content }}</th>
-      <th>
-        <form action="/article/{{ $article->id }}" method="POST">
-        {{ csrf_field() }}
-        {{ method_field('DELETE') }}
-        <button>Delete</button>
-        </form>
-      </th>
-    </tr>
-  @endforeach
-</table>
+            <div id="article"></div>
+        </div>
+    </div>
+    <script src="{{ asset('js/app.js')}}"></script>
 
 @endsection
