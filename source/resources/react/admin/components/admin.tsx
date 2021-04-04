8438c90c2a3a65a18ee87520a979c99ee2a9ad7b
shota12085 <shota.toyoshima007@gmail.com>
2021-04-04 23:28:33 +0900

    adminページ作成

--- source/resources/react/admin/components/admin.tsx
+++ source/resources/react/admin/components/admin.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { TopPage } from './toppage';
+import { Login } from './login';
 import { LeftSide } from './left_side';
 import {Row, Col} from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch, redirect } from "react-router-dom";
 
 
 export const Admin: React.FC = (): JSX.Element => {
   return (
-    <Row>
-      <Col sm={3} style={{background:"#fff"}}>
-          <LeftSide />
-      </Col>
-      <Col sm={9}>
-
-      </Col>
-    </Row>
+    <Router>
+      <Row style={{height:"100vh"}}>
+        <Col sm={3} style={{background:"#fff"}}>
+            <LeftSide />
+        </Col>
+        <Col sm={9}>
+            <Switch>
+              <Route path="/admin" component={TopPage} exact />
+              <Route path="/admin/login" component={Login} exact />
+            </Switch>
+        </Col>
+      </Row>
+    </Router>
   );
 };
